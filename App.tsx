6580d289294bf11824ef7b4c781e2ea5ca6072df
Osarugue Enehizena <nehirugue@gmail.com>
2024-06-13 23:12:17 +0100

    without array of data

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.box}>
+        <Text style={styles.text}>1</Text>
+      </View>
+
+      <View style={styles.box}>
+        <Text style={styles.text}>2</Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+
+    gap: 10,
+  },
+  box: {
+    backgroundColor: "#F9EDE3",
+    width: 300,
+    height: 300,
+
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "#9b4521",
+    borderRadius: 20,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#9b4521",
+    fontSize: 70,
   },
 });
